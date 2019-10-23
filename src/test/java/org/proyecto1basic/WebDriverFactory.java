2e2fdfbdf97bce4f995271a704e26b6bbcab4a9d
AngelinaGuzman <gelo0412@gmail.com>
2019-10-22 19:00:27 -0500

    uno

---
+++ src/test/java/org/proyecto1basic/WebDriverFactory.java
@@ -0,0 +1,31 @@
+package org.proyecto1basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+
+public class WebDriverFactory {
+
+        public static WebDriver getDriver(String browser) {
+            File rootPath = new File("drivers");
+            if(browser.toLowerCase().equals("chrome")) {
+                File chromeFilePath = new File(rootPath, "chromedriver.exe");
+                ChromeOptions options= new ChromeOptions();
+                options.addArguments("--disable-notifications");
+                options.addArguments("--lang=es-mx");
+                System.setProperty("webdriver.chrome.driver", chromeFilePath.getPath());
+                return new ChromeDriver(options);
+            } else if (browser.toLowerCase().equals("firefox")) {
+                File firefoxFilePath = new File(rootPath, "geckodriver.exe");
+                System.setProperty("webdriver.gecko.driver", firefoxFilePath.getPath());
+                return new FirefoxDriver();
+            } else {
+                return null;
+            }
+        }
+    }
+
+
